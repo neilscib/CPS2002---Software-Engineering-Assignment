5daf2216e97accb95ba2468401cf3c2552c9ed6f
franklyn07 <franklyn.sciberras@gmail.com>
2018-05-19 20:52:34 +0200

    Fixed percentage of Hazardous map

---
+++ src/main/java/FlagWatcher.java
@@ -0,0 +1,48 @@
+/*import java.util.ArrayList;
+import java.util.Set;
+
+public class FlagWatcher {
+
+    boolean [] flagArray;
+    ArrayList<Observer> observers;
+    ArrayList<Set<Integer>> teamPlayers;
+    Set<Integer> multipleWinners; //index of winner players
+
+    public FlagWatcher(boolean[] flagArray)
+    {
+        this.flagArray = new boolean[flagArray.length];
+
+        //arraycopy(src,srcPosition,des,desPosition,length)
+        System.arraycopy(flagArray, 0, this.flagArray, 0, flagArray.length);
+
+    }
+
+    public void register(Observer newObserver, int teamNumber)
+    {
+        observers.add(newObserver);
+
+        teamPlayers.get(teamNumber).add(observers.size()-1);
+    }
+
+    public void unregister(Observer observerToDelete)
+    {
+        int obsIndex = observers.indexOf(observerToDelete);
+        observers.remove(obsIndex);
+    }
+
+    public void notifyObservers(Set<Integer> multipleWinners)
+    {
+        for(int i = 0; i < observers.size(); i++)
+        {
+            if (multipleWinners.contains(i) )
+            {
+                observers.get(i).update();
+                //update() will set the local win from false to true
+            }
+
+
+        }
+    }
+
+}*/
+
--- src/main/java/Map_Factory.java
+++ src/main/java/Map_Factory.java
@@ -1,12 +1,16 @@
+import java.util.Random;
+
 public class Map_Factory {
 
-    private static final int HAZARDOUS = 35;
-    private static final int SAFE = 25;
+    private Random rand = new Random();
+
+    private int HAZARDOUS = rand.nextInt(35)+25;
+    private static final int SAFE = 10;
 
     //pass string type
     public Map getMap(String map_type){
         //return type of map according to parameter
-        //prbablities are listed as static variables
+        //probablities are listed as static variables
         if(map_type.equals("hazardous")){
             return Map.getMapInstance(HAZARDOUS);
         }else if(map_type.equals("safe")){
